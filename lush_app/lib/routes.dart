4ff8f599d62881469530849e40614a0b8cdbab2c
Simone Boerci <boercisimone@gmail.com>
2024-06-11 22:11:47 +0200

    Login screen created

--- lush_app/lib/routes.dart
+++ lush_app/lib/routes.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:lush_app/screens/fast_registration_screen.dart';
 import 'package:lush_app/screens/first_step_verification_screen.dart';
+import 'package:lush_app/screens/login_screen.dart';
 import 'package:lush_app/screens/second_step_verification_screen.dart';
 import 'package:lush_app/screens/third_step_verification_screen.dart';
 import 'package:lush_app/screens/verification_completed_screen.dart';
 import 'screens/complete_registration_screen.dart';
 
 Map<String, WidgetBuilder> routes = {
-  '/': (context) => VerificationCompletedScreen(),
+  '/': (context) => LoginScreen(),
   '/fast_registration': (context) => FastRegistrationScreen(),
   '/complete_registration': (context) => RegistrationScreen(),
   '/first_step_verification_screen': (context) => FirstStepVerificationScreen(),
   '/second_step_verification_screen': (context) =>
       SecondStepVerificationScreen(),
   '/third_step_verification_screen': (context) => ThirdStepVerificationScreen(),
-  '/verification_completed_screen': (context) => VerificationCompletedScreen(),
+  '/verification_completed_screen': (context) =>
+      const VerificationCompletedScreen(),
+  '/login_screen': (context) => LoginScreen(),
 };
---
+++ lush_app/lib/screens/login_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:lush_app/widgets/custom_background.dart';
+import 'package:lush_app/widgets/registration_header.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:lush_app/widgets/custom_form.dart';
+import 'package:lush_app/widgets/custom_elevated_button.dart';
+import 'package:lush_app/widgets/custom_text_field.dart';
+
+import 'package:lush_app/models/fast_registration_view_model.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => FastRegistrationViewModel(),
+      child: CustomBackground(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const RegistrationHeader(
+              text: 'Accedi al tuo account',
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomForm(
+                  formKey: _formKey,
+                  textFields: [
+                    CustomTextField.large(
+                      hintText: 'La mia email',
+                      onChanged: (value) {},
+                    ),
+                    CustomTextField.large(
+                      hintText: 'La mia password',
+                      onChanged: (value) {},
+                    ),
+                  ],
+                  button: CustomElevatedButton(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    text: 'Entra',
+                    onPressed: () {},
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'Oppure',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                CustomElevatedButton.google(
+                  text: 'Accedi con Google',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
