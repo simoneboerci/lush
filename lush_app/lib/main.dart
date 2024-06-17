36e836a3a40f66bd5bbf3490bd54b50ac216dd9d
Simone Boerci <boercisimone@gmail.com>
2024-06-17 11:35:16 +0200

    Flutter firebase added

--- lush_app/lib/main.dart
+++ lush_app/lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() async {
+    await initFirebase();
+    super.initState();
+  }
+
+  Future<void> initFirebase() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
